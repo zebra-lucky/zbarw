beb3fb4df1ae3819ae128936baa4813fa5b27036
Jarek Czekalski <jarekczek@poczta.onet.pl>
2012-08-23 08:06:41 +0200

    Fixed unclosed comment in Processor.h (bug since 402:f0269fb73f22)

--- include/zbar/Processor.h
+++ include/zbar/Processor.h
@@ -70,7 +70,7 @@ class Processor {
     /// see zbar_processor_init()
     /** @param enable_display Whether to prepare the window for displaying
       *        output. If `true`, this call should be followed by
-      *        `set_visible` to put the window into correct state.
+      *        `set_visible` to put the window into correct state. */
     void init (const char *video_device = "",
                bool enable_display = true)
     {
